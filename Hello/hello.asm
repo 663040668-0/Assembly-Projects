d919eff6e8b1eebb7e2b82bab736909cceb28033
Sudthipod Wanaprom <sudthipod.w@kkumail.com>
2023-12-23 09:36:14 +0700

    readibility approved

--- Hello/hello.asm
+++ Hello/hello.asm
@@ -1,20 +1,18 @@
 ;----------------------------------------------------------------------------------------
 ;
-; This is a FASM assembly program which show a message box "Hello, Assembly!" on run
+; This is a FASM assembly program which show a message box "Hello, Assembly!" on run.
 ;
 ;----------------------------------------------------------------------------------------
 
-include '%fasminc%\win64ax.inc'   ; Use extended ASCII header for Windows 64-bit
+include '%fasminc%\win64ax.inc'                                                                         ; Use extended ASCII header for Windows 64-bit
 
-.code ; Start the code section
+.code                                                                                                   ; Start the code section
 
-  _start: ; Create _start function (in this case, main function)
-    invoke MessageBox, HWND_DESKTOP, "Hello, Assembly!", "This is my first assembly program.", MB_OK
-    ; Represented --> Procedure, params... (HWND_DESKTOP means the handle window of desktop, use to draw things to desktop. next is text, title, and messageBox type)
+  _start:                                                                                               ; Create _start function (in this case, main function)
+    invoke MessageBox, HWND_DESKTOP, "Hello, Assembly!", "This is my first assembly program.", MB_OK    ; Represented --> Procedure, params... (HWND_DESKTOP means the handle window of desktop, use to draw things to desktop. next is text, title, and messageBox type)
 
-    invoke ExitProcess, 0
-    ; Exit the program with code 0 which mean "successful execution"
+    invoke ExitProcess, 0                                                                               ; Exit the program with code 0 which mean "successful execution"
 
-  .end _start ; End of the function
+  .end _start                                                                                           ; End of the function
 
 ; Run the program with provided launch.json with F5 Anything extension
